938b26e733af98d1bf94c0f0cf108f73da9ac7a2
304-5\Administrator <111475594+yyy32443123@users.noreply.github.com>
2023-02-09 10:24:00 +0900

    보드 수정 aop 추가

--- src/main/java/com/green/nowon/domain/entity/member/AddressEntityRepsoitory.java
+++ src/main/java/com/green/nowon/domain/entity/member/AddressEntityRepsoitory.java
@@ -3,7 +3,6 @@ package com.green.nowon.domain.entity.member;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface AddressEntityRepsoitory extends JpaRepository<AddressEntity, Long> {
 
