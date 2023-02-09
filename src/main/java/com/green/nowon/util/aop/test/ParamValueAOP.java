877abec16d622dab846a7294302e885b9ce43bca
304-5\Administrator <111475594+yyy32443123@users.noreply.github.com>
2023-02-09 13:09:50 +0900

    aop 개선 2

--- src/main/java/com/green/nowon/util/aop/test/ParamValueAOP.java
+++ src/main/java/com/green/nowon/util/aop/test/ParamValueAOP.java
@@ -15,16 +15,13 @@ import lombok.extern.log4j.Log4j2;
 // @formatter:off
 @Log4j2 @Aspect @Component
 public class ParamValueAOP {
-@Around("execution(* *..*Controller.*(..)) || paramV() || execution(* *com.green.nowon.*.*Service.*(..))")public Object aop(final ProceedingJoinPoint joinPoint) throws Throwable {
-  final var stopWatch = new StopWatch();
-  stopWatch.start();
+@Around("execution(* *com.green.nowon.*.*Controller.*(..)) || paramV() || execution(* *com.green.nowon.*.*Service.*(..))")public Object aop(final ProceedingJoinPoint joinPoint) throws Throwable {
+  final var stopWatch = new StopWatch(); stopWatch.start();
   StringBuilder sb = new StringBuilder("\n\n--------------------------------------------------------------\n");
   Arrays.asList(joinPoint.getArgs()).forEach(arg -> {
-    if (arg.getClass().getSimpleName().isEmpty()) return;
-  sb.append("\n            {}.{}의 파라미터\n")
-  .append("\n            데이터타입: {} \n            값: {}\n");
-  log.debug(sb.toString()
-  , joinPoint.getTarget().getClass().getSimpleName(),
+    if (arg.getClass().getSimpleName()!=""&&arg!="")
+    sb.append("\n            메서드 : {}.{}").append("\n            타입: {}").append("\n            값: {}\n");
+  log.debug(sb.toString(), joinPoint.getTarget().getClass().getSimpleName(),
   ((MethodSignature) joinPoint.getSignature()).getMethod().getName(), arg.getClass().getSimpleName(), arg);
   });
   final var result = joinPoint.proceed();
@@ -34,10 +31,8 @@ public class ParamValueAOP {
   final var extMethod = ((MethodSignature) joinPoint.getSignature()).getMethod().getName();
   final var extClass = joinPoint.getTarget().getClass().getSimpleName();
   if (result != null) log.debug(sb.append("\n\n============================================================\n")
-  .append("\n            {} 형\n            반환 : {}\n            실행시간 : {}.{}({}ms)\n            ")
+  .append("\n            타입 : {}").append("\n            반환 : {}").append("\n            실행시간 : {}.{}({}ms)\n")
   .append("\n============================================================\n").toString(), 
-  result.getClass().getSimpleName(), result, extClass, extMethod, msTot);
-  return result;
-}
-@Pointcut("@annotation(com.green.nowon.util.aop.test.ParamVal)")private void paramV() {}}
+  result.getClass().getSimpleName(), result, extClass, extMethod, msTot);return result;
+}@Pointcut("@annotation(com.green.nowon.util.aop.test.ParamVal)")private void paramV() {}}
 // @formatter:on
