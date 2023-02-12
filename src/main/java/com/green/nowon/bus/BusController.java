c4dccf0283b427f8829389be2b897e60c1153888
YoungJuLee <46048175+wykaw@users.noreply.github.com>
2023-02-12 12:27:04 +0900

    2023-02-12 KAKAO MAP 기능 부분

--- src/main/java/com/green/nowon/bus/BusController.java
+++ src/main/java/com/green/nowon/bus/BusController.java
@@ -1,9 +1,15 @@
 package com.green.nowon.bus;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.green.nowon.bus.dto.ArriveRequestDTO;
+import com.green.nowon.bus.dto.BusArriveItem;
 
 @RestController
 public class BusController {
@@ -18,6 +24,31 @@ public class BusController {
 		return new ModelAndView("bus/bus-index");
 	}
 
+	@GetMapping("/bus/arriveInfo")
+	public List<BusArriveItem> arriveInfo(ArriveRequestDTO dto) {
+		return busService.arriveInfo(dto);
+	}
+	
+	private <T>String ObjectToString(T target) {
+		String result=null;
+		if(target instanceof Number) {
+			result=target.toString();
+		}else if(target instanceof String) {
+			result=(String)target;
+			result=result.replace(",",",\n");
+			result=result.replace("{","{\n ");
+			result=result.replace("(","(\n ");
+			result=result.replace("}","\n}");
+			result=result.replace(")","\n)");
+		}else {
+			System.out.println("문자열도 숫자도아님");
+			result=ObjectToString(target.toString());
+		}
+		
+		return result;
+		
+	}
+	
 	// ajax 요청
 	@GetMapping("/bus/search")
 	public ModelAndView busSearch(String strSrch, ModelAndView mv) {
@@ -26,4 +57,18 @@ public class BusController {
 		return mv;
 	}
 
+	@GetMapping("/bus/{busRouteId}")
+	public ModelAndView getStaionsByRouteList(@PathVariable String busRouteId, ModelAndView mv) {
+		mv.setViewName("bus/station-list");
+		busService.getStaionsByRouteList(busRouteId, mv);
+		return mv;
+	}
+	
+	@GetMapping("/placesSearch")
+	public ModelAndView placesSearch(String search, ModelAndView mv) {
+		mv.setViewName("bus/list");
+		busService.getPlacesSearch(search, mv);
+		return mv;
+	}
+	
 }
